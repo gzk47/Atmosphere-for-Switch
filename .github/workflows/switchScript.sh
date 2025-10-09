ec4db51fa582d5624b8a799c59f55cd7192f35f0
gzk47 <98312059+gzk47@users.noreply.github.com>
2025-10-09 09:43:05 +0800

    Update switchScript.sh

--- .github/workflows/switchScript.sh
+++ .github/workflows/switchScript.sh
@@ -527,7 +527,7 @@ else
 fi
 
 ### Fetch lastest ReverseNX-Tool from https://github.com/masagrator/ReverseNX-Tool/releases
-curl -H "$API_AUTH" -o latest.json -sL https://api.github.com/repos/masagrator/ReverseNX-Tool/releases/latest
+curl -H "$API_AUTH" -o latest.json -sL https://api.github.com/repos/gzk47/ReverseNX-Tool/releases/latest
 cat latest.json \
   | jq '.tag_name' \
   | xargs -I {} echo ReverseNX-Tool {} >> ../description.txt
